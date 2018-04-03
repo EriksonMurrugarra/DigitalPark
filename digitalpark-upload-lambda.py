203c84e123d8f821ce49f69bdbc850bb1d39f0aa
Erikson Murrugarra <erikson.murrugarra@gmail.com>
2018-04-02 23:30:36 -0500

    Sending SNS when deployed

--- digitalpark-upload-lambda.py
+++ digitalpark-upload-lambda.py
@@ -3,20 +3,28 @@ import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
+    sns = boto3.resource('sns')
 
-#declare buckets
-digitalpark_bucket = s3.Bucket('www.digitalpark.io')
-build_bucket = s3.Bucket('digitalpark-build')
+    #declare SNS
+    topic = sns.Topic('arn:aws:sns:us-east-1:470187922491:DeployDigitalParkTopic')
 
-#download from build bucket
-digitalpark_zip = StringIO.StringIO()
-build_bucket.download_fileobj('digitalpark.zip', digitalpark_zip)
+    #declare buckets
+    digitalpark_bucket = s3.Bucket('www.digitalpark.io')
+    build_bucket = s3.Bucket('digitalpark-build')
 
-#push to production bucket
-with zipfile.ZipFile(digitalpark_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        digitalpark_bucket.upload_fileobj(obj, nm,
-          ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        digitalpark_bucket.Object(nm).Acl().put(ACL='public-read')
+    #download from build bucket
+    digitalpark_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('digitalpark.zip', digitalpark_zip)
+
+    #push to production bucket
+    with zipfile.ZipFile(digitalpark_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            digitalpark_bucket.upload_fileobj(obj, nm,
+              ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            digitalpark_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    topic.publish(Subject='DigitalPark Deployment Status. SUCCESS', Message='DigitalPark Deployment successfully!')
+    return 'DigitalPark Deployed!'
