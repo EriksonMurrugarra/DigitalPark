bff1ce198c1030c7374b2f1236c3ce42bd25ae6a
Erikson Murrugarra <erikson.murrugarra@gmail.com>
2018-04-03 22:55:40 -0500

    Adding change

--- digitalpark-upload-lambda.py
+++ digitalpark-upload-lambda.py
@@ -4,20 +4,34 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3')
-    sns = boto3.resource('sns')
-
     #declare SNS
+    sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:470187922491:DeployDigitalParkTopic')
 
+    location = {
+      "bucketName": "digitalpark-build",
+      "objectKey": "digitalpark.zip"
+    }
+
     try:
+      #extract from codepipeline
+      job = event.get('CodePipeline.job')
+
+      if job:
+        for artifact in job["data"]["inputArtifacts"]:
+          if artifact["name"] == "MyAppBuild":
+            location = artifact["location"]["s3Location"]
+
+      print "Building DigitalPark from " + str(location)
+
       #declare buckets
+      s3 = boto3.resource('s3')
       digitalpark_bucket = s3.Bucket('www.digitalpark.io')
-      build_bucket = s3.Bucket('digitalpark-build')
+      build_bucket = s3.Bucket(location["bucketName"])
 
       #download from build bucket
       digitalpark_zip = StringIO.StringIO()
-      build_bucket.download_fileobj('digitalpark.zip', digitalpark_zip)
+      build_bucket.download_fileobj(location["objectKey"], digitalpark_zip)
 
       #push to production bucket
       with zipfile.ZipFile(digitalpark_zip) as myzip:
@@ -28,6 +42,12 @@ def lambda_handler(event, context):
               digitalpark_bucket.Object(nm).Acl().put(ACL='public-read')
 
       topic.publish(Subject='DigitalPark Deployment Status. SUCCESS', Message='DigitalPark Deployment successfully!')
+
+      #codepipeline finish
+      if job:
+        codepipeline = boto3.client('codepipeline')
+        codepipeline.put_job_success_result(jobId=job["id"])
+
       return 'DigitalPark Deployed!'
     except:
       topic.publish(Subject='DigitalPark Deployment Status. FAILED', Message='DigitalPark Deployment failed!')
--- src/header/components/header.js
+++ src/header/components/header.js
@@ -3,7 +3,7 @@ import Search from '../../search/components/search';
 
 const Header = () => (
   <div>
-    <h1> DigitalPark </h1>
+    <h1> DigitalPark v2</h1>
     <h2> Que quieres aprender Hoy? </h2>
     <Search />
   </div>
